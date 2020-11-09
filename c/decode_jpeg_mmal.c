579b01831257d81e6cb77d780b5cd89580d1257c
Ivan Usalko <i.usalko@itransition.com>
2020-11-09 17:52:13 +0300

    # INIT-1
    Add debug information

--- c/decode_jpeg_mmal.c
+++ c/decode_jpeg_mmal.c
@@ -332,15 +332,14 @@ DECODING_RESULT_T* decode_jpeg_mmal(char *file_path, bool use_mmap, bool debug_i
          // buffer->length = fread(buffer->data, 1, buffer->alloc_size - 128, source_file);
 
          read_bytes = MIN(not_read_bytes, buffer->alloc_size - 128);
+         printf("OK 21 read bytes %d\n", read_bytes);
          if (read_bytes > 0)
          {
-            printf("OK 21 read bytes %d", read_bytes);
             not_read_bytes -= read_bytes;
             memcpy(buffer->data, mapped, read_bytes);
             buffer->length = read_bytes;
          }
          else {
-            printf("OK 21 read bytes %d", read_bytes);
             buffer->length = 0;
          }
          buffer->offset = 0;
