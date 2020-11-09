87fb8691cc3610b07671f9ef8c67edf96e87e243
Ivan Usalko <i.usalko@itransition.com>
2020-11-09 09:28:20 +0300

    # INIT-1
    Implement read mapped file

--- c/read_file_with_mmap.c
+++ c/read_file_with_mmap.c
@@ -4,5 +4,42 @@ READING_RESULT_T* read_file(char *file_path)
 {
     READING_RESULT_T *result = malloc(sizeof(READING_RESULT_T));
     result->length = 0;
+
+    int fd;
+    struct stat s;
+    const char * mapped;
+    int i;
+
+    fd = open(file_path, O_RDONLY);
+    if (fd < 0)
+    {
+        result->errors = strerror(errno);
+        return result;
+    }
+
+    if (fstat(fd, &s) < 0)
+    {
+        result->errors = strerror(errno);
+        return result;
+    }
+
+    mapped = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (mapped == MAP_FAILED)
+    {
+        result->errors = strerror(errno);
+        return result;
+    }
+
+    result->data = malloc(s.st_size);
+    memcpy(result, mapped, s.st_size);
+    result->length = s.st_size;
+
+    if (munmap(mapped , s.st_size) < 0)
+    {
+        free(result->data);
+        result->errors = strerror(errno);
+        return result;
+    }
+
     return result;
 }
--- c/read_file_with_mmap.h
+++ c/read_file_with_mmap.h
@@ -1,7 +1,13 @@
 #ifndef READ_FILE_WITH_MMAP_H
 #define READ_FILE_WITH_MMAP_H
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/errno.h>
+#include <fcntl.h>
+#include <string.h>
 
 typedef struct READING_RESULT_T {
     void* data;
